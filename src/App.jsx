cc2c7190186ff00bc5c5e82c5bd0e3fefe12c478
mereluo <meredithluo710@gmail.com>
2023-12-12 22:09:52 -0800

    Make username as route direction

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
 
     async function handleLogin(username, password) {
         try {
-            const response = await fetch("http://localhost:3500/api/user/login", {
+            const response = await fetch("api/user/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
