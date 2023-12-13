a719353179eedd76d5b025f833177be1e04c1906
xiaolamb <54784964+xiaolamb@users.noreply.github.com>
2023-12-13 13:40:20 -0800

    add css files to style pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,46 +3,49 @@ import { Link, useNavigate } from "react-router-dom";
 import "../styling/Navbar.css";
 
 const Navbar = ({ isLoggedIn, handleLogout, username }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const performLogout = () => {
-        handleLogout();
-        navigate("/login");
-    };
+  const performLogout = () => {
+    handleLogout();
+    navigate("/login");
+  };
 
-    return (
-        <nav className="navbar">
-            <Link to="/home" className="navbar-link">
-                Home
-            </Link>
+  return (
+    <nav className="navbar">
+      <Link to="/home" className="navbar-link">
+        Home
+      </Link>
 
-            {isLoggedIn ? (
-                <div className="auth-links">
-                    <Link className="navbar-link navbar-username" to={`/user/${username}`}>
-                        {username}
-                    </Link>
-                    <button onClick={performLogout} className="navbar-link">
-                        Logout
-                    </button>
-                </div>
-            ) : (
-                <div className="auth-links">
-                    <Link to="/login" className="navbar-link">
-                        Log In
-                    </Link>
-                    <Link to="/register" className="navbar-link">
-                        Sign Up
-                    </Link>
-                </div>
-            )}
-        </nav>
-    );
+      {isLoggedIn ? (
+        <div className="auth-links">
+          <Link
+            className="navbar-link navbar-username"
+            to={`/user/${username}`}
+          >
+            {username}
+          </Link>
+          <button onClick={performLogout} className="navbar-link">
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div className="auth-links">
+          <Link to="/login" className="navbar-link">
+            Log In
+          </Link>
+          <Link to="/register" className="navbar-link">
+            Sign Up
+          </Link>
+        </div>
+      )}
+    </nav>
+  );
 };
 
 Navbar.propTypes = {
-    isLoggedIn: PropTypes.bool.isRequired,
-    handleLogout: PropTypes.func.isRequired,
-    username: PropTypes.string,
+  isLoggedIn: PropTypes.bool.isRequired,
+  handleLogout: PropTypes.func.isRequired,
+  username: PropTypes.string,
 };
 
 export default Navbar;
--- src/components/TweetForm.jsx
+++ src/components/TweetForm.jsx
@@ -1,51 +1,55 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "../styling/TweetForm.css";
 
 const TweetForm = ({ setIsTweetChange }) => {
-    const [content, setContent] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
+  const [content, setContent] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const handleInputChange = (event) => {
-        setContent(event.target.value);
-    };
+  const handleInputChange = (event) => {
+    setContent(event.target.value);
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        try {
-            await axios.post("/api/tweet", { content });
-            setIsTweetChange((prevCount) => prevCount + 1);
-            setContent(""); // Clear the input field after successful submission
-            setErrorMessage("");
-        } catch (error) {
-            if (error.response) {
-                setErrorMessage(error.response.data);
-            } else {
-                setErrorMessage("Something went wrong. Please try again.");
-            }
-        }
-    };
+    try {
+      await axios.post("/api/tweet", { content });
+      setIsTweetChange((prevCount) => prevCount + 1);
+      setContent(""); // Clear the input field after successful submission
+      setErrorMessage("");
+    } catch (error) {
+      if (error.response) {
+        setErrorMessage(error.response.data);
+      } else {
+        setErrorMessage("Something went wrong. Please try again.");
+      }
+    }
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="content">Tweet:</label>
-                    <input
-                        type="text"
-                        id="content"
-                        value={content}
-                        onChange={handleInputChange}
-                        required
-                        placeholder="What's happening?"
-                        style={{ width: "200px", height: "300px" }} // Adjust the width as needed
-                    />
-                </div>
-                <button type="submit">Create Tweet</button>
-            </form>
-            {errorMessage && <p>{errorMessage}</p>}
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="input-box">
+          <label htmlFor="content">Tweet:</label>
+          <div className="input-container">
+            <input
+              type="text"
+              id="content"
+              value={content}
+              onChange={handleInputChange}
+              required
+              placeholder="What's happening?"
+              style={{ width: "400px", height: "200px" }}
+            />
+          </div>
         </div>
-    );
+
+        <button type="submit">Create Tweet</button>
+      </form>
+      {errorMessage && <p>{errorMessage}</p>}
+    </div>
+  );
 };
 
 export default TweetForm;
