e38f9b2155f28da07d2d7837d6e30e00861f203c
mereluo <meredithluo710@gmail.com>
2023-12-13 14:51:05 -0800

    Update tweet style and app style

--- server/db/tweet.schema.js
+++ server/db/tweet.schema.js
@@ -1,20 +1,18 @@
 const Schema = require("mongoose").Schema;
 
 exports.TweetSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+        },
+        createdTime: {
+            type: Date,
+            default: Date.now,
+        },
     },
-    content: {
-      type: String,
-      // minLength: 1,
-      // maxLength: 100,
-    },
-    createdTime: {
-      type: Date,
-      default: Date.now,
-    },
-  },
-  { collection: "tweetTable" },
+    { collection: "tweetTable" }
 );
