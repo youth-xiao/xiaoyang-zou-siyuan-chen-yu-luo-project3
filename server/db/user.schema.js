73f6d64b3b6d3f3ce1622a70e04ea66740e1c2ff
xiaolamb <54784964+xiaolamb@users.noreply.github.com>
2023-12-13 18:14:26 -0800

    run prettier on jsx and js files

--- server/db/user.schema.js
+++ server/db/user.schema.js
@@ -1,24 +1,24 @@
 const Schema = require("mongoose").Schema;
 
 exports.UserSchema = new Schema(
-    {
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String,
-            default: "",
-        },
-        createdTime: {
-            type: Date,
-            default: Date.now,
-        },
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
     },
-    { collection: "userTable" }
+    password: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    createdTime: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { collection: "userTable" },
 );
--- server/tweet.server.js
+++ server/tweet.server.js
@@ -7,38 +7,38 @@ const TweetAccessor = require("./db/tweet.model");
  * Logged in user can create new tweets
  */
 router.post("/", async function (request, response) {
-    const username = request.cookies.username;
-    if (!username) {
-        response.status(400);
-        return response.send("Users need to be logged in to create a new tweet.");
-    }
+  const username = request.cookies.username;
+  if (!username) {
+    response.status(400);
+    return response.send("Users need to be logged in to create a new tweet.");
+  }
 
-    const content = request.body.content;
-    if (!content) {
-        response.status(400);
-        return response.send("Missing tweet content.");
-    }
+  const content = request.body.content;
+  if (!content) {
+    response.status(400);
+    return response.send("Missing tweet content.");
+  }
 
-    const newTweet = {
-        username: username,
-        content: content,
-    };
+  const newTweet = {
+    username: username,
+    content: content,
+  };
 
-    const createdTweet = await TweetAccessor.insertTweet(newTweet);
-    response.json(createdTweet);
+  const createdTweet = await TweetAccessor.insertTweet(newTweet);
+  response.json(createdTweet);
 });
 
 /**
  * Get all available tweets on home page
  */
 router.get("/", async function (request, response) {
-    try {
-        const foundTweets = await TweetAccessor.getAllTweet();
-        return response.json(foundTweets);
-    } catch (error) {
-        response.status(500);
-        return response.send("Error fetching tweets.");
-    }
+  try {
+    const foundTweets = await TweetAccessor.getAllTweet();
+    return response.json(foundTweets);
+  } catch (error) {
+    response.status(500);
+    return response.send("Error fetching tweets.");
+  }
 });
 
 /**
@@ -47,18 +47,19 @@ router.get("/", async function (request, response) {
  * e.g., /api/tweet/xiaoyang ==> return all tweets owned by xiaoyang
  */
 router.get("/:username", async function (request, response) {
-    const requestedUsername = request.params.username;
-    if (!requestedUsername) {
-        response.status(400);
-        return response.send("Username parameter is missing.");
-    }
-    try {
-        const foundTweets = await TweetAccessor.findTweetByUsername(requestedUsername);
-        return response.json(foundTweets);
-    } catch (error) {
-        response.status(500);
-        return response.send("Error fetching user's tweets.");
-    }
+  const requestedUsername = request.params.username;
+  if (!requestedUsername) {
+    response.status(400);
+    return response.send("Username parameter is missing.");
+  }
+  try {
+    const foundTweets =
+      await TweetAccessor.findTweetByUsername(requestedUsername);
+    return response.json(foundTweets);
+  } catch (error) {
+    response.status(500);
+    return response.send("Error fetching user's tweets.");
+  }
 });
 
 /**
@@ -67,22 +68,22 @@ router.get("/:username", async function (request, response) {
  * everyone can get every tweet
  */
 router.get("/id/:tweetId", async function (request, response) {
-    const tweetId = request.params.tweetId;
-    if (!tweetId) {
-        response.status(400);
-        return response.send("Tweet ID parameter is missing.");
+  const tweetId = request.params.tweetId;
+  if (!tweetId) {
+    response.status(400);
+    return response.send("Tweet ID parameter is missing.");
+  }
+  try {
+    const foundTweet = await TweetAccessor.findTweetById(tweetId);
+    if (!foundTweet) {
+      response.status(404);
+      return response.send("Tweet not found.");
     }
-    try {
-        const foundTweet = await TweetAccessor.findTweetById(tweetId);
-        if (!foundTweet) {
-            response.status(404);
-            return response.send("Tweet not found.");
-        }
-        return response.json(foundTweet);
-    } catch (error) {
-        response.status(500);
-        return response.send("Error fetching tweet.");
-    }
+    return response.json(foundTweet);
+  } catch (error) {
+    response.status(500);
+    return response.send("Error fetching tweet.");
+  }
 });
 
 /**
@@ -90,77 +91,90 @@ router.get("/id/:tweetId", async function (request, response) {
  * only when the user is logged in, they have access to edit their tweets
  */
 const checkTweetOwnership = async function (request, response, next) {
-    const tweetId = request.params.tweetId;
-    const username = request.cookies.username;
-    if (!username) {
-        response.status(401);
-        return response.send("User not logged in.");
-    }
-    try {
-        const foundTweet = await TweetAccessor.findTweetById(tweetId);
+  const tweetId = request.params.tweetId;
+  const username = request.cookies.username;
+  if (!username) {
+    response.status(401);
+    return response.send("User not logged in.");
+  }
+  try {
+    const foundTweet = await TweetAccessor.findTweetById(tweetId);
 
-        if (!foundTweet) {
-            response.status(404);
-            return response.send("Tweet not found.");
-        }
-        if (foundTweet.username !== username) {
-            response.status(403);
-            return response.send("You do not have permission to perform this action.");
-        }
-        next(); // If ownership is confirmed, proceed to the next handler
-    } catch (error) {
-        response.status(500);
-        return response.send("Error checking tweet ownership.");
+    if (!foundTweet) {
+      response.status(404);
+      return response.send("Tweet not found.");
     }
+    if (foundTweet.username !== username) {
+      response.status(403);
+      return response.send(
+        "You do not have permission to perform this action.",
+      );
+    }
+    next(); // If ownership is confirmed, proceed to the next handler
+  } catch (error) {
+    response.status(500);
+    return response.send("Error checking tweet ownership.");
+  }
 };
 
 /**
  * Delete tweet by ID
  * e.g., /api/tweet/:tweetId => delete tweet with :tweetId
  */
-router.delete("/id/:tweetId", checkTweetOwnership, async function (request, response) {
+router.delete(
+  "/id/:tweetId",
+  checkTweetOwnership,
+  async function (request, response) {
     const tweetId = request.params.tweetId;
     if (!tweetId) {
-        response.status(400);
-        return response.send("Tweet ID parameter is missing.");
+      response.status(400);
+      return response.send("Tweet ID parameter is missing.");
     }
 
     try {
-        const deletedTweet = await TweetAccessor.deleteTweetById(tweetId);
-        if (!deletedTweet) {
-            response.status(404);
-            return response.send("Tweet not found.");
-        }
-        return response.json(deletedTweet);
+      const deletedTweet = await TweetAccessor.deleteTweetById(tweetId);
+      if (!deletedTweet) {
+        response.status(404);
+        return response.send("Tweet not found.");
+      }
+      return response.json(deletedTweet);
     } catch (error) {
-        response.status(500);
-        return response.send("Error deleting tweet.");
+      response.status(500);
+      return response.send("Error deleting tweet.");
     }
-});
+  },
+);
 
 /**
  * Update tweet by ID
  * e.g., /api/tweet/:tweetId => update tweet with :tweetId
  */
-router.put("/id/:tweetId", checkTweetOwnership, async function (request, response) {
+router.put(
+  "/id/:tweetId",
+  checkTweetOwnership,
+  async function (request, response) {
     const tweetId = request.params.tweetId;
     const updatedContent = request.body.content;
     if (!tweetId || !updatedContent) {
-        response.status(400);
-        return response.send("Tweet ID or updated content is missing.");
+      response.status(400);
+      return response.send("Tweet ID or updated content is missing.");
     }
 
     try {
-        const updatedTweet = await TweetAccessor.updateTweetById(tweetId, updatedContent);
-        if (!updatedTweet) {
-            response.status(404);
-            return response.send("Tweet not found.");
-        }
-        return response.json(updatedTweet);
+      const updatedTweet = await TweetAccessor.updateTweetById(
+        tweetId,
+        updatedContent,
+      );
+      if (!updatedTweet) {
+        response.status(404);
+        return response.send("Tweet not found.");
+      }
+      return response.json(updatedTweet);
     } catch (error) {
-        response.status(500);
-        return response.send("Error updating tweet.");
+      response.status(500);
+      return response.send("Error updating tweet.");
     }
-});
+  },
+);
 
 module.exports = router;
